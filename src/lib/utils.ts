04918905778ac75ad42594680325eb4a220425e5
tsmialek <tsmialek.1@gmail.com>
2025-03-06 20:30:47 +0100

    feat: add useColorMode utility for managing light and dark themes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,43 @@
-import type { Updater } from '@tanstack/vue-table'
-import type { Ref } from 'vue'
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import type { Updater } from '@tanstack/vue-table';
+import type { Ref } from 'vue';
+import { computed, ref, watch } from 'vue';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
-  ref.value
-    = typeof updaterOrValue === 'function'
+export function valueUpdater<T extends Updater<any>>(
+  updaterOrValue: T,
+  ref: Ref
+) {
+  ref.value =
+    typeof updaterOrValue === 'function'
       ? updaterOrValue(ref.value)
-      : updaterOrValue
+      : updaterOrValue;
+}
+
+export function useColorMode() {
+  const colorMode = ref('light');
+
+  const getColorMode = computed(() => colorMode.value);
+  function setColorMode(mode: string) {
+    if (mode === 'light' || mode === 'dark') colorMode.value = mode;
+    else console.warn('Unsupported color mode provided');
+  }
+
+  function applyColorMode(mode: string): void {
+    document.documentElement.classList.remove('light', 'dark');
+    document.documentElement.classList.add(mode);
+  }
+
+  watch(colorMode, (newMode: string) => {
+    applyColorMode(newMode);
+  });
+
+  return {
+    colorMode: getColorMode,
+    setColorMode,
+  };
 }
