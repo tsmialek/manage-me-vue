7d5fef45e54c1e6ededf9ffac5297013ceae78a1
tsmialek <tsmialek.1@gmail.com>
2025-07-02 22:38:53 +0200

    feat: Import drawer components

--- frontend/src/pages/TaskDetails.vue
+++ frontend/src/pages/TaskDetails.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import router from '@/router';
 import { onMounted, onUnmounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 import { useActiveTaskStore, useTaskStore } from '@/store';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardTitle, CardHeader, CardContent } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
+import { PlusIcon } from 'lucide-vue-next';
+import { Toaster } from '@/components/ui/toast';
 
 const activeTaskStore = useActiveTaskStore();
 const taskStore = useTaskStore();
@@ -28,7 +36,12 @@ onUnmounted(async () => {
 </script>
 
 <template>
-  <div>{{ activeTaskStore.activeTaskId }}</div>
+  <Card class="grid grid-cols-[1fr_auto] p-4">
+    <h1 class="text-2xl font-semibold justify-self-center">
+      {{ activeTaskStore.activeTask?.title }}
+    </h1>
+    <Button @click="router.go(-1)" class="justify-self-end">Back</Button>
+  </Card>
 </template>
 
 <style scoped></style>
